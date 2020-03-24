564ff06e06f5c3c3c58a7da3f26c6144504d294f
Okeremeta Jude Tega <okeremeta.tega@gmail.com>
2020-03-24 17:15:05 +0100

    [finishes #171956253] Setup continuous integration

---
+++ src/tests/playground.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+
+describe('calculate', () => {
+  it('add', function() {
+    const result = 9 + 8
+    expect(result).to.equal(17);
+  }); 
+});
