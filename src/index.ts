0cdf2c82d517e054b3b132bbf9bee6a3a118a323
Okeremeta Jude Tega <okeremeta.tega@gmail.com>
2020-03-25 18:27:34 +0100

    [finishes #171962707] Setup database and models

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,53 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
-import {User} from "./entity/User";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import {config}from 'dotenv';
+import * as bcrypt from "bcryptjs";
+import routes from "./routes";
+import { User } from "./entity/User";
+
+config()
+
+const {
+    ADMINPASSWORD
+} = process.env
 
 createConnection().then(async connection => {
-
     // create express app
-    const app = express();
+    const app: express.Application = express();
+
+    // Call midlewares
+    app.use(cors());
+    app.use(helmet());
     app.use(bodyParser.json());
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
-    });
-
-    // setup express app here
-    // ...
+    //Set all routes from routes folder
+    app.use("/", routes);
 
     // start express server
     app.listen(3000);
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
+    // check admins
+    let userRepository = connection.getRepository(User);
+    let adminUser = await userRepository.findOne({ username: "adminOne", });
+
+    let hashedPassword = bcrypt.hashSync(ADMINPASSWORD, 8)
+
+    // insert admin users for test
+    if (!adminUser) {
+        await connection.manager.save(connection.manager.create('User', {
+            username: "adminOne",
+            password: hashedPassword,
+            role: "admin"
+        }));
+        await connection.manager.save(connection.manager.create('User', {
+            username: "adminTwo",
+            password: hashedPassword,
+            role: "admin"
+        }));
+    }
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
